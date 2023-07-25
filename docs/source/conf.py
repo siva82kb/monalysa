de45ff80a3876b72fa36b06fe30168664150368b
Sivakumar Balasubramanian <siva82kb@gmail.com>
2023-07-25 21:52:54 +0530

    Added an import statement to the conf.py file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,9 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+sys.path.add("../../../")
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
