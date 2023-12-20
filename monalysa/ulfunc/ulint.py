corrected average_intuse
computing number of samples for the rolling window was wrong

monalysa/ulfunc/ulint.py
@@ -77,8 +77,8 @@ def average_intuse(intsig: np.array, usesig: np.array, windur: float,
     assert np.all(np.any(np.array([np.array(intsig) >= 0,
                                    np.isnan(intsig)]), axis=0)), "Use signal must be a binary signal."
     
-    n_win = int(windur / sample_t)
-    n_shift = int(winshift / sample_t)
+    n_win = int(windur * sample_t)
+    n_shift = int(winshift * sample_t)
     _avgint = signal.lfilter(b=np.ones(n_win), a=np.array([n_win]), x=intsig)
     _avguse = signal.lfilter(b=np.ones(n_win), a=np.array([n_win]), x=usesig)
     _avgintuse = np.array([_ai / _au if _au > 0